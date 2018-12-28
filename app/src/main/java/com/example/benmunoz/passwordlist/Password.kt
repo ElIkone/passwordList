10491dca56744cf176ff9ad8b946f79472ca4606
Ikone <elmaildebenja@gmail.com>
2018-12-27 19:57:00 -0800

    Feat: Add password class

---
+++ app/src/main/java/com/example/benmunoz/passwordlist/Password.kt
@@ -0,0 +1,11 @@
+package com.example.benmunoz.passwordlist
+
+class Password {
+    var id: Int
+    var title: String
+
+    constructor(title: String, id: Int) {
+        this.title = title
+        this.id = id
+    }
+}
