a2f2c6b929dfa055febe7b2b0e1ba412fba23562
benjaminMunoz <elmaildebenja@gmail.com>
2018-12-27 16:35:16 -0800

    Add initial files

---
+++ app/src/main/java/com/example/benmunoz/passwordlist/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.benmunoz.passwordlist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
