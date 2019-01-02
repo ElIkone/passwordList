8f3dfcf1fde25f385b0e9caddf0cfc1f3805aa9a
benjaminMunoz <elmaildebenja@gmail.com>
2019-01-01 21:11:14 -0800

    Feat: Add function to items to the arraylist

--- app/src/main/java/com/example/benmunoz/passwordlist/MainActivity.kt
+++ app/src/main/java/com/example/benmunoz/passwordlist/MainActivity.kt
@@ -2,11 +2,35 @@ package com.example.benmunoz.passwordlist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val password: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addPassword()
+        password_list.layoutManager = LinearLayoutManager(this)
+    }
+
+    fun addPassword() {
+        password.add("test123")
+        password.add("test123")
+        password.add("test123")
+        password.add("test123")
+        password.add("test123")
+        password.add("test123")
+        password.add("test123")
+        password.add("test123")
+        password.add("test123")
+        password.add("test123")
+        password.add("test123")
+        password.add("test123")
+        password.add("test123")
+        password.add("test123")
+        password.add("test123")
+        password.add("test123")
     }
 }
